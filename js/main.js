8a9bbd307a77138b1f398afcd244eab3ccc19b01
arudenko <rudandrzej@gmail.com>
2022-10-25 22:42:16 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let myById = document.getElementById("opis");
+let myButton = document.querySelector("button")
+myButton.onclick = function () {
+    myById.textContent = "Here goes the full description"
+}
